cb695632f46495d3d2e6a5ffcd3f9b38e2ce3ee8
FireLord1 <firelord.xda@gmail.com>
2016-09-27 01:50:17 +0530

    msm8916-common: update media stuff according to kyo tree
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- product/media.mk
+++ product/media.mk
@@ -3,8 +3,22 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true
+	media.stagefright.enable-player=true \
+	media.stagefright.enable-http=true \
+	media.stagefright.enable-aac=true \
+	media.stagefright.enable-qcp=true \
+	media.stagefright.enable-fma2dp=true \
+	media.stagefright.enable-scan=true \
+	media.msm8939hw=0 \
+	media.msm8929hw=0 \
+	mm.enable.smoothstreaming=true \
+	mmp.enable.3g2=true \
+	media.aac_51_output_enabled=true
+
