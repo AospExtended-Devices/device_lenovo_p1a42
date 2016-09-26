402d78832b3f700d416dfbafb833b2be67ff857f
FireLord1 <firelord.xda@gmail.com>
2016-09-27 01:48:17 +0530

    msm8916-common: add fingerprint stuff
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

---
+++ product/fingerprint.mk
@@ -0,0 +1,8 @@
+# Fingerprint sensor
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    fingerprint.msm8916 \
+    fingerprintd
+
