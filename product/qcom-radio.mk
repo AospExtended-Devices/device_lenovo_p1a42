7a5ab038266e05d5588bfaea7c025d2fbc4d296b
FireLord1 <firelord.xda@gmail.com>
2016-09-27 03:34:36 +0530

    msm8916-common: add radio ralated lenovo stuff from kyo
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,3 +1,18 @@
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.custom_ecc=1 \
+    persist.radio.ecc_hard_1=998 \
+    persist.radio.ecc_hard_count=1 \
+    persist.radio.multisim.config=dsds \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ro.telephony.default_network=9,1
+
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
@@ -13,7 +28,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
     persist.radio.apm_sim_not_pwdn=1 \
     ro.telephony.call_ring.multiple=false \
-    ro.use_data_netmgrd=true
+    ro.use_data_netmgrd=true \
+    persist.radio.sib16_support=1
 
 # RIL
 PRODUCT_PACKAGES += \
