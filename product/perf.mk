f4a0914d69fffce49620d0b20c2173a5f76fc1b3
FireLord1 <firelord.xda@gmail.com>
2016-10-16 16:22:52 +0530

    msm8916-common: remove msm8916 hook and add some stock props
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- product/perf.mk
+++ product/perf.mk
@@ -1,9 +1,6 @@
 # Properties
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.dex2oat_thread_count=4
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.max_starting_bg=8 \
-    ro.sys.fw.use_trim_settings=true
+    ro.sys.fw.use_trim_settings=true \
+    ro.product.hw.version=H201 \
