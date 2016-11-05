f26523caf9450849036078ac52b96a0945ff0dc2
FireLord1 <firelord.xda@gmail.com>
2016-11-05 21:15:42 +0530

    Revert "msm8916-common : Switch to prebuilt liblights"
    This reverts commit f323e8f99ec9222f845acf4de1f00a6edc222cb1.
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -12,13 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+LOCAL_PATH:= $(call my-dir)
+# HAL module implemenation stored in
+# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
-LOCAL_MODULE := liblights.msm8916
-LOCAL_MODULE_OWNER := lenovo
-LOCAL_SRC_FILES_64 := lib64/liblights.msm8916.so
-LOCAL_SRC_FILES_32 := lib/liblights.msm8916.so
-LOCAL_MULTILIB := both
+
+LOCAL_SRC_FILES := lights.c
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_CFLAGS := $(common_flags) -DLOG_TAG=\"qdlights\"
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
+
+include $(BUILD_SHARED_LIBRARY)
