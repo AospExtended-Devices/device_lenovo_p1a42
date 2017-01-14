CyanogenMod/android_device_yu_tomato

tomato: cmhw: Update and clean up KeyDisabler
 * Update copyright
 * Use new CMSDK FileUtils
 * Mark String constant as static final
 * Actually check the file path in isSupported()
 * Prevent null pointer exceptions when reading the node
 * Clean up formatting

Change-Id: Ief64e211fcfecbb220cf871dea8af53478932b2f

cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2014-2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -26,19 +26,21 @@ import org.cyanogenmod.hardware.util.FileUtils;
  * really should not be using this on a device with mechanical or
  * otherwise visible-when-inactive keys
  */
-
 public class KeyDisabler {
 
-    private static String CONTROL_PATH = "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-005d/keypad_enable";
+    private static final String KEYPAD_PATH =
+            "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-005d/keypad_enable";
 
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return FileUtils.isFileWritable(KEYPAD_PATH) &&
+                FileUtils.isFileReadable(KEYPAD_PATH);
+    }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return "0".equals(FileUtils.readOneLine(KEYPAD_PATH));
     }
 
     public static boolean setActive(boolean state) {
-        return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
+        return FileUtils.writeLine(KEYPAD_PATH, state ? "0" : "1");
     }
-
 }

AospExtended-Devices/device_lenovo_p1a42

 * Mark String constants as static final
Signed-off-by: FireLord1 <firelord.xda@gmail.com>

cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2014-2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
@@ -26,19 +26,20 @@ import org.cyanogenmod.hardware.util.FileUtils;
  * really should not be using this on a device with mechanical or
  * otherwise visible-when-inactive keys
  */
-
 public class KeyDisabler {
+    private static final String CONTROL_PATH =
+            "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-005d/keypad_enable";
 
-    private static String CONTROL_PATH = "/sys/devices/soc.0/78b9000.i2c/i2c-5/5-005d/keypad_enable";
-
-    public static boolean isSupported() { return true; }
+    public static boolean isSupported() {
+        return FileUtils.isFileReadable(CONTROL_PATH) &&
+                FileUtils.isFileWritable(CONTROL_PATH);
+    }
 
     public static boolean isActive() {
-        return (FileUtils.readOneLine(CONTROL_PATH).equals("0"));
+        return "0".equals(FileUtils.readOneLine(CONTROL_PATH));
     }
 
     public static boolean setActive(boolean state) {
-        return FileUtils.writeLine(CONTROL_PATH, (state ? "0" : "1"));
+        return FileUtils.writeLine(CONTROL_PATH, state ? "0" : "1");
     }
-
 }