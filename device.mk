50d6a3af903a561bc518ab9d322553efa99ed148
FireLord1 <firelord.xda@gmail.com>
2017-02-25 15:29:52 +0530

    p1a42: Update kernel path to copy wifi files
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- device.mk
+++ device.mk
@@ -200,5 +200,5 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    kernel/lenovo/msm8916/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    kernel/lenovo/msm8916/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+    kernel/lenovo/p1a42/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    kernel/lenovo/p1a42/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
