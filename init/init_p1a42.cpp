8ba1f41c8a7a6e214b4f7ca51ef8ec9e9c0ae997
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-08-06 18:46:51 +0530

    p1a42: Allow vendor init to override props
    Change-Id: Ic7c4d86616bfe33b9668b5b54d119d0380f4660c
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- init/init_p1a42.cpp
+++ init/init_p1a42.cpp
@@ -34,11 +34,24 @@
 #include <fstream>
 #include <string>
 #include <sys/sysinfo.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
 int is3GB()
 {
